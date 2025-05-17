9fb4deba9544be70e052a2e3354dbbdd7721a2a5
gracyy17 <septimogracey@gmail.com>
2025-05-18 00:01:35 +0800

    Add main.js file

---
+++ main.js
@@ -0,0 +1,16 @@
+let menu = document.querySelector("#menu-bars");
+        let navbar = document.querySelector(".navbar");
+
+        menu.onclick = () => {
+            menu.classList.toggle('fa-times');
+            navbar.classList.toggle('active');
+        };
+
+        var swiper = new Swiper(".home-slider", {
+            loop: true,
+            grabCursor: true,
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+        });
