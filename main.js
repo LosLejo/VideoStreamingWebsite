23039a7045163bad64616a45bb3ffa9ae0f02e07
gracyy17 <septimogracey@gmail.com>
2025-05-18 00:22:08 +0800

    Update main.js with latest changes

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 let menu = document.querySelector("#menu-bars");
-        let navbar = document.querySelector(".navbar");
+let navbar = document.querySelector(".navbar");
 
-        menu.onclick = () => {
-            menu.classList.toggle('fa-times');
-            navbar.classList.toggle('active');
-        };
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
 
-        var swiper = new Swiper(".home-slider", {
-            loop: true,
-            grabCursor: true,
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-            },
-        });
+var swiper = new Swiper(".home-slider", {
+    loop: true,
+    grabCursor: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false, 
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
