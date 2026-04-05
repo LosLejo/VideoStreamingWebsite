0b59c8e7a4b44cfd5fcd02120ad7ea66bc6fa44f
Loss <carlos.alejo0603@gmail.com>
2026-04-05 15:51:20 +0800

    Security: Move database credentials to environment variables

--- config.php
+++ config.php
@@ -16,10 +16,21 @@ $dotenv->load();
 $dotenv->required([
     'GOOGLE_CLIENT_ID',
     'GOOGLE_CLIENT_SECRET',
-    'GOOGLE_REDIRECT_URI'
+    'GOOGLE_REDIRECT_URI',
+    'DB_HOST',
+    'DB_USER',
+    'DB_PASSWORD',
+    'DB_NAME'
 ])->notEmpty();
 
 // Export configuration as constants for easy access
+// Google OAuth
 define('GOOGLE_CLIENT_ID', $_ENV['GOOGLE_CLIENT_ID']);
 define('GOOGLE_CLIENT_SECRET', $_ENV['GOOGLE_CLIENT_SECRET']);
 define('GOOGLE_REDIRECT_URI', $_ENV['GOOGLE_REDIRECT_URI']);
+
+// Database
+define('DB_HOST', $_ENV['DB_HOST']);
+define('DB_USER', $_ENV['DB_USER']);
+define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
+define('DB_NAME', $_ENV['DB_NAME']);
